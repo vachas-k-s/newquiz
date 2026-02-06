679679fea47f94012ef6da9a73724c0976e09aab
vachas-k-s <vachaskavuvila@gmail.com>
2026-02-06 08:21:54 +0530

    Add files via upload

---
+++ script_updated.js
@@ -0,0 +1,652 @@
+/*QUESTIONS OBJECT */
+
+const questions = {
+  physics: [
+    {
+      question: "Unit of force is?",
+      options: ["Newton", "Joule", "Watt", "Pascal"],
+      ans: 0,
+      userSelection: null,
+    },
+    {
+      question: "Speed of light is?",
+      options: ["3×10⁸ m/s", "300 m/s", "3×10⁶ m/s", "150 m/s"],
+      ans: 0,
+      userSelection: null,
+    },
+    {
+      question: "SI unit of power?",
+      options: ["Watt", "Joule", "Volt", "Ampere"],
+      ans: 0,
+      userSelection: null,
+    },
+    {
+      question: "Which is a vector quantity?",
+      options: ["Speed", "Distance", "Velocity", "Mass"],
+      ans: 2,
+      userSelection: null,
+    },
+    {
+      question: "Sound cannot travel in?",
+      options: ["Air", "Water", "Steel", "Vacuum"],
+      ans: 3,
+      userSelection: null,
+    },
+    {
+      question: "SI unit of pressure?",
+      options: ["Newton", "Pascal", "Bar", "Joule"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "Which mirror forms real image?",
+      options: ["Plane", "Convex", "Concave", "None"],
+      ans: 2,
+      userSelection: null,
+    },
+    {
+      question: "Unit of frequency?",
+      options: ["Second", "Hertz", "Meter", "Watt"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "Energy stored in spring?",
+      options: ["Kinetic", "Elastic", "Heat", "Nuclear"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "Heat transfer without medium?",
+      options: ["Conduction", "Convection", "Radiation", "Reflection"],
+      ans: 2,
+      userSelection: null,
+    },
+    {
+      question: "SI unit of work?",
+      options: ["Watt", "Newton", "Joule", "Pascal"],
+      ans: 2,
+      userSelection: null,
+    },
+    {
+      question: "Which ray has no charge?",
+      options: ["Alpha", "Beta", "Gamma", "Electron"],
+      ans: 2,
+      userSelection: null,
+    },
+    {
+      question: "Who discovered gravity?",
+      options: ["Einstein", "Newton", "Galileo", "Tesla"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "Unit of electric current?",
+      options: ["Volt", "Ampere", "Ohm", "Watt"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "Convex lens is used for?",
+      options: ["Myopia", "Hypermetropia", "Astigmatism", "Blindness"],
+      ans: 1,
+      userSelection: null,
+    },
+  ],
+
+  chemistry: [
+    {
+      question: "Symbol of Hydrogen?",
+      options: ["H", "He", "Hy", "Hd"],
+      ans: 0,
+      userSelection: null,
+    },
+    {
+      question: "pH of neutral solution?",
+      options: ["5", "7", "0", "14"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "Common salt formula?",
+      options: ["NaCl", "KCl", "Na₂CO₃", "CaCl₂"],
+      ans: 0,
+      userSelection: null,
+    },
+    {
+      question: "Gas used in photosynthesis?",
+      options: ["O₂", "CO₂", "N₂", "H₂"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "Atomic number of Oxygen?",
+      options: ["6", "7", "8", "16"],
+      ans: 2,
+      userSelection: null,
+    },
+    {
+      question: "Which is an acid?",
+      options: ["NaOH", "HCl", "KOH", "NaCl"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "Formula of water?",
+      options: ["HO", "H₂O", "H₂O₂", "OH"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "Rust is caused by?",
+      options: ["Water", "Oxygen", "Both", "None"],
+      ans: 2,
+      userSelection: null,
+    },
+    {
+      question: "Liquid metal?",
+      options: ["Iron", "Gold", "Mercury", "Silver"],
+      ans: 2,
+      userSelection: null,
+    },
+    {
+      question: "Lime water turns milky due to?",
+      options: ["O₂", "CO₂", "N₂", "H₂"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "Atomic number of Carbon?",
+      options: ["6", "12", "14", "8"],
+      ans: 0,
+      userSelection: null,
+    },
+    {
+      question: "Which is a base?",
+      options: ["H₂SO₄", "HNO₃", "NaOH", "HCl"],
+      ans: 2,
+      userSelection: null,
+    },
+    {
+      question: "Hardest natural substance?",
+      options: ["Iron", "Diamond", "Gold", "Granite"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "Bleaching powder used for?",
+      options: ["Cooking", "Cleaning", "Bleaching", "Heating"],
+      ans: 2,
+      userSelection: null,
+    },
+    {
+      question: "Gas causing acid rain?",
+      options: ["CO₂", "SO₂", "O₂", "N₂"],
+      ans: 1,
+      userSelection: null,
+    },
+  ],
+
+  biology: [
+    {
+      question: "Basic unit of life?",
+      options: ["Cell", "Tissue", "Organ", "System"],
+      ans: 0,
+      userSelection: null,
+    },
+    {
+      question: "Photosynthesis occurs in?",
+      options: ["Mitochondria", "Chloroplast", "Nucleus", "Ribosome"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "Largest organ in body?",
+      options: ["Heart", "Skin", "Liver", "Brain"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "Blood is pumped by?",
+      options: ["Brain", "Lungs", "Heart", "Kidney"],
+      ans: 2,
+      userSelection: null,
+    },
+    {
+      question: "Vitamin from sunlight?",
+      options: ["A", "B", "C", "D"],
+      ans: 3,
+      userSelection: null,
+    },
+    {
+      question: "Plant pores are?",
+      options: ["Roots", "Veins", "Stomata", "Lenticels"],
+      ans: 2,
+      userSelection: null,
+    },
+    {
+      question: "Largest bone?",
+      options: ["Skull", "Femur", "Arm", "Rib"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "Respiration organ?",
+      options: ["Heart", "Lungs", "Liver", "Kidney"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "DNA found in?",
+      options: ["Cytoplasm", "Cell wall", "Nucleus", "Ribosome"],
+      ans: 2,
+      userSelection: null,
+    },
+    {
+      question: "Largest animal?",
+      options: ["Elephant", "Blue Whale", "Shark", "Giraffe"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "Blood color?",
+      options: ["Blue", "Green", "Red", "Yellow"],
+      ans: 2,
+      userSelection: null,
+    },
+    {
+      question: "Which filters blood?",
+      options: ["Heart", "Kidney", "Liver", "Lungs"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "Sense organ?",
+      options: ["Liver", "Eye", "Heart", "Kidney"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "Plants make food by?",
+      options: ["Respiration", "Digestion", "Photosynthesis", "Transpiration"],
+      ans: 2,
+      userSelection: null,
+    },
+    {
+      question: "Blood group discovered by?",
+      options: ["Darwin", "Newton", "Karl Landsteiner", "Pasteur"],
+      ans: 2,
+      userSelection: null,
+    },
+  ],
+
+  gk: [
+    {
+      question: "Capital of India?",
+      options: ["Mumbai", "Delhi", "Kolkata", "Chennai"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "National bird?",
+      options: ["Crow", "Peacock", "Eagle", "Parrot"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "Largest ocean?",
+      options: ["Atlantic", "Indian", "Pacific", "Arctic"],
+      ans: 2,
+      userSelection: null,
+    },
+    {
+      question: "Father of Nation?",
+      options: ["Nehru", "Gandhi", "Patel", "Bose"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "Currency of Japan?",
+      options: ["Won", "Yuan", "Yen", "Dollar"],
+      ans: 2,
+      userSelection: null,
+    },
+    {
+      question: "Red planet?",
+      options: ["Venus", "Mars", "Jupiter", "Saturn"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "National animal?",
+      options: ["Lion", "Tiger", "Elephant", "Leopard"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "Largest continent?",
+      options: ["Africa", "Europe", "Asia", "Australia"],
+      ans: 2,
+      userSelection: null,
+    },
+    {
+      question: "Taj Mahal is in?",
+      options: ["Delhi", "Agra", "Jaipur", "Lucknow"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "Independence Day?",
+      options: ["Jan 26", "Aug 15", "Oct 2", "Nov 14"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "National flower?",
+      options: ["Rose", "Lotus", "Lily", "Sunflower"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "Inventor of computer?",
+      options: ["Newton", "Einstein", "Charles Babbage", "Tesla"],
+      ans: 2,
+      userSelection: null,
+    },
+    {
+      question: "Largest desert?",
+      options: ["Gobi", "Thar", "Sahara", "Arabian"],
+      ans: 2,
+      userSelection: null,
+    },
+    {
+      question: "Indian flag colors?",
+      options: ["2", "3", "4", "5"],
+      ans: 1,
+      userSelection: null,
+    },
+    {
+      question: "Who wrote Ramayana?",
+      options: ["Valmiki", "Vyasa", "Tulsidas", "Kalidas"],
+      ans: 0,
+      userSelection: null,
+    },
+  ],
+};
+
+/*OTHER VARIABLES*/
+let studentName, rollNo, subject, qnsNo;
+let currentQuestion = 0;
+let quizData = [];
+let timer;
+
+
+/*IMPORTANT DOMELEMENTS WHICH IS REUSED MORE THAN ONCE*/
+const optionBtns = Array.from(document.getElementById("options").children);
+
+const nextButton = document.getElementById("nextButton");
+const prevButton = document.getElementById("prevButton");
+const submitButton = document.getElementById("submitButton");
+const clearButton = document.getElementById("clearButton");
+
+const welcomePage = document.getElementById("welcomePage");
+const subjectPage = document.getElementById("subjectPage");
+const progressPage = document.getElementById("ProgressPage");
+const noOfQnsPage = document.getElementById("noOfQnsPage");
+const resultPage = document.getElementById("resultPage");
+const quizPage = document.getElementById("quizPage");
+const question = document.getElementById("question");
+let progressBar = null;
+
+/*FUNCTION TO SHOW SUBJECTS LIST*/
+function showSubjects() {
+  studentName = document.getElementById("studentName").value;
+  rollNo = document.getElementById("rollNo").value;
+
+  if (studentName === "" || rollNo === "") {
+    alert("ENTER THE DETAILS!!!!");
+  } else {
+    welcomePage.classList.add("hidden");
+    subjectPage.classList.remove("hidden");
+  }
+}
+
+/*FUNCTION TO READ SUBJECT*/
+
+function fetchSubject(s) {
+  subject = s;
+  subjectPage.classList.add("hidden");
+  noOfQnsPage.classList.remove("hidden");
+}
+
+function startQuiz(qns) {
+  qnsNo = qns;
+  quizData = questions[subject].slice(0, qnsNo);
+
+  noOfQnsPage.classList.add("hidden");
+  quizPage.classList.remove("hidden");
+
+  startTimer();
+  showProgressBarAtQuizTime();
+  loadQuestion();
+}
+
+function showProgressBarAtQuizTime() {
+  progressPage.classList.add("grid");
+  for (let i = 0; i < qnsNo; i++) {
+    const qnBox = document.createElement("button");
+    qnBox.style.border = "none";
+    qnBox.innerText = `${i + 1}`;
+    qnBox.onclick = () => {
+      currentQuestion = i;
+      loadQuestion();
+    };
+    progressPage.appendChild(qnBox);
+  }
+  progressBar = Array.from(progressPage.children);
+}
+
+function loadQuestion() {
+  // Get the current question object
+  let qObj = quizData[currentQuestion];
+
+  // Display the question text
+  question.innerText = `Q${currentQuestion + 1}. ${qObj.question}`;
+
+  // Disable "Previous" and "Next" buttons on the first and last questions
+
+  prevButton.style.pointerEvents = currentQuestion === 0 ? "none" : "auto";
+  nextButton.style.pointerEvents =
+    currentQuestion === qnsNo - 1 ? "none" : "auto";
+
+  const currentQuestionBar = progressBar[currentQuestion];
+
+  currentQuestionBar.style.background = "grey";
+  // Loop through all option buttons
+  if (qObj.userSelection !== null)
+    currentQuestionBar.style.background = "#0c9449";
+  optionBtns.forEach((btn, index) => {
+    // Set option text
+    btn.innerHTML = qObj.options[index];
+
+    // Reset option styles and enable clicking
+    btn.style.background = "";
+    btn.style.pointerEvents = "auto";
+
+    // Restore previously selected option (when navigating back)
+    if (qObj.userSelection === index) btn.style.background = "#c3376f";
+
+    // Handle option click
+    btn.onclick = () => {
+      // Save user's selection in question object
+      qObj.userSelection = index;
+      currentQuestionBar.style.background = "#0c9449";
+      // Reset all option buttons
+      optionBtns.forEach((i) => {
+        i.style.background = "";
+        i.style.pointerEvents = "auto";
+      });
+
+      // Highlight selected option and lock it
+      btn.style.background = "#c3376f";
+      btn.style.pointerEvents = "none";
+    };
+  });
+
+  // Clear selected option for the current question
+  clearButton.onclick = () => {
+    qObj.userSelection = null;
+    currentQuestionBar.style.background = "#0c9449";
+    optionBtns.forEach((i) => {
+      i.style.background = "";
+      i.style.pointerEvents = "auto";
+    });
+  };
+
+  // Navigation button handlers
+  nextButton.onclick = nextQuestion;
+  prevButton.onclick = prevQuestion;
+  submitButton.onclick = submitAnswers;
+}
+
+function startTimer() {
+  clearInterval(timer);
+  let timeLeft = qnsNo * 10;
+
+  document.getElementById("timer").innerText = `Time Left: ${timeLeft}s`;
+  timer = setInterval(() => {
+    timeLeft--;
+    document.getElementById("timer").innerText = `Time Left: ${timeLeft}s`;
+    if (timeLeft <= 0) {
+      clearInterval(timer);
+      showResult();
+    }
+  }, 1000);
+}
+
+function submitAnswers() {
+  const answeredCount = quizData.filter((q) => q.userSelection !== null).length;
+  if (
+    confirm(`YOU HAVE ANSWERED ONLY ${answeredCount}/${qnsNo} QUESTIONS!!!`)
+  ) {
+    clearInterval(timer);
+    showResult();
+  } 
+}
+
+function nextQuestion() {
+  currentQuestion++;
+  if (currentQuestion < quizData.length) loadQuestion();
+}
+function prevQuestion() {
+  currentQuestion--;
+  if (currentQuestion > -1) loadQuestion();
+}
+
+function showResult() {
+  quizPage.classList.add("hidden");
+  resultPage.classList.remove("hidden");
+  let score = 0;
+  quizData.forEach((q) => {
+    if (q.ans === q.userSelection) score++;
+  });
+
+  let percentage = (score / quizData.length) * 100;
+  let grade;
+  if (percentage >= 90) grade = "A+ (Excellent)";
+  else if (percentage >= 75) grade = "A (Very Good)";
+  else if (percentage >= 65) grade = "B (Good)";
+  else if (percentage >= 40) grade = "C (Average)";
+  else grade = "F (Needs Improvement)";
+
+  document.getElementById("studentDetails").innerText =
+    `Name: ${studentName}\n Roll No: ${rollNo}`;
+  document.getElementById("score").innerText =
+    `Score: ${score} / ${quizData.length} (${percentage.toFixed(2)}%)`;
+
+  document.getElementById("grade").innerText = `Grade: ${grade}`;
+  showProgressBarAfterQuiz();
+}
+
+function showProgressBarAfterQuiz() {
+  submitButton.style.pointerEvents = "none";
+  clearButton.style.pointerEvents = "none";
+
+  progressBar.forEach((btn, index) => {
+    const qObj = quizData[index];
+    if (qObj.userSelection !== null)
+      if (qObj.ans === qObj.userSelection)
+        btn.style.background = "rgb(6, 230, 6)";
+      else btn.style.background = "red";
+    else btn.style.background = "orange";
+    btn.onclick = () => {
+      currentQuestion = index;
+      loadQnAfterQuiz();
+    };
+  });
+}
+
+function loadQnAfterQuiz() {
+  // Get the current question object
+  const qObj = quizData[currentQuestion];
+  quizPage.classList.remove("hidden");
+  // Display the question text
+  question.innerText = `Q${currentQuestion + 1}. ${qObj.question}`;
+
+  // Disable "Previous" button on the first question
+  prevButton.style.pointerEvents = currentQuestion === 0 ? "none" : "auto";
+  nextButton.style.pointerEvents =
+    currentQuestion === qnsNo - 1 ? "none" : "auto";
+
+  // Loop through all option buttons
+  optionBtns.forEach((btn, index) => {
+    // Set option text
+    btn.innerHTML = qObj.options[index];
+
+    // Reset option styles and enable clicking
+    btn.style.background = "";
+    btn.style.pointerEvents = "none";
+  });
+  if (qObj.userSelection !== null)
+    optionBtns[qObj.userSelection].style.background = "red";
+  optionBtns[qObj.ans].style.background = "rgb(6, 230, 6)";
+
+  // Navigation button handlers
+  nextButton.onclick = nextQuestionAfterQuiz;
+  prevButton.onclick = prevQuestionAfterQuiz;
+
+  console.log(nextButton.onclick);
+}
+
+function nextQuestionAfterQuiz() {
+  currentQuestion++;
+  if (currentQuestion < quizData.length) loadQnAfterQuiz();
+}
+function prevQuestionAfterQuiz() {
+  currentQuestion--;
+  if (currentQuestion > -1) loadQnAfterQuiz();
+}
+
+// Theme toggle
+const toggleBtn = document.getElementById("themeToggle");
+if (toggleBtn) {
+  const savedTheme = localStorage.getItem("theme") || "light";
+  document.body.classList.add(savedTheme);
+
+  toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+    document.body.classList.toggle("light");
+    const theme = document.body.classList.contains("dark") ? "dark" : "light";
+    localStorage.setItem("theme", theme);
+  });
+}
+
+// Date & Time
+function updateDateTime() {
+  const dt = document.getElementById("datetime");
+  if (!dt) return;
+  const now = new Date();
+  dt.innerText = now.toLocaleString();
+}
+setInterval(updateDateTime, 1000);
+updateDateTime();
